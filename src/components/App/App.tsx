971f23181766e3d2f54ca3229cbb70be11188667
ihor <i0962112822@gmail.com>
2024-01-22 21:22:39 +0200

    add router

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+
+interface AppProps {}
+
+export default function App({}: AppProps) {
+	return (
+		<>
+			<Routes> </Routes>
+		</>
+	);
+}
