596e0301463f4294db2d40dc835b520964832318
ihor <i0962112822@gmail.com>
2024-01-22 21:42:17 +0200

    add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 
 interface AppProps {}
 
 export default function App({}: AppProps) {
 	return (
 		<>
-			<Routes> </Routes>
+      <Routes> 
+        <Route path="/" element={""}>
+				<Route index element={""} />
+      </Routes>
 		</>
 	);
 }
